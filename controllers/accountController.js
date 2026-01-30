9db046ddd1834caab9e353b18f1440ac33b7b5d4
El-Luih <luisalejandro632@gmail.com>
2026-01-30 06:35:32 -0600

    W04 L - Registration MVC and POST processing.

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,5 @@
 const utilities = require('../utilities');
+const accountModel = require('../models/account-model')
 
 const accountController = {}
 
@@ -24,6 +25,37 @@ accountController.buildRegistration = async function (req, res, next) {
         nav,
         errors: null
     })
+}
+
+accountController.registerAccount = async function (req, res, next) {
+    let nav = await utilities.getNav()
+    const { account_firstname, account_lastname, account_email, account_password } = req.body
+    const registrationResults = accountModel.postAccount(
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+
+    if (registrationResults) {
+        req.flash(
+            "notice",
+            `Congratualtions, you\'re registered ${account_firstname}. Please log in.`
+        )
+        res.status(201).render("account/login", {
+            title: "Login",
+            nav
+        })
+    } else {
+        req.flash(
+            "notice",
+            "Sorry, the registration failed."
+        )
+        res.status(501).render("account/register", {
+            title: "Registration",
+            nav
+        })
+    }
 }
 
 module.exports = accountController;
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,7 +3,10 @@ const router = express.Router()
 const accountController = require('../controllers/accountController')
 const utilities = require('../utilities')
 
+//GET REQUESTS
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/registration", utilities.handleErrors(accountController.buildRegistration))
+//POST REQUESTS
+router.post("/registration", utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router;
