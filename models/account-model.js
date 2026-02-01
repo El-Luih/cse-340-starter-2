95eedfd7850cc9a9b26105fc602a6429da19cfaf
El-Luih <luisalejandro632@gmail.com>
2026-02-01 06:58:08 -0600

    W04 L - Stickiness, Email check and Password hashing.

--- models/account-model.js
+++ models/account-model.js
@@ -11,5 +11,18 @@ accountModel.postAccount = async function (account_firstname, account_lastname, 
   }
 }
 
+/* **********************
+ *   Check for existing email
+ * ********************* */
+accountModel.checkExistingEmail = async function (account_email) {
+    try {
+    const sql = "SELECT * FROM account WHERE account_email = $1"
+    const email = await pool.query(sql, [account_email])
+    return email.rowCount
+    } catch (error) {
+        return error.message
+    }
+}
+
 module.exports = accountModel
 
